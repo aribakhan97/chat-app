4da1cced5b659d9a8fbfd10390642f5fedb25e93
aribakhan97 <akhan41@binghamton.edu>
2021-02-22 14:35:46 -0500

    created components

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { ChatEngine } from 'react-chat-engine';
+import ChatFeed from './components/ChatFeed';
+import './App.css';
+
+const App = () => {
+    return (
+        <ChatEngine
+            height="100vh"
+            projectID="b38fb56b-6be7-4492-9a79-404f88ee7c56"
+            userName="coder"
+            userSecret="123123"
+            renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+        />
+    )
+}
+
+export default App;
